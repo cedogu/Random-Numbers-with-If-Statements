39809c5229cd750fcff7c1770ca720f9c15bfe4a
cedogu <86116050+cedogu@users.noreply.github.com>
2022-09-18 15:35:56 +0300

    Add project files.

---
+++ Random Number if statements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Random_Number_if_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+
+            int number = randomNumber.Next(0, 50);
+
+            Console.WriteLine(number);
+            
+
+            if (number < 20)
+            {
+                Console.WriteLine("Random Number is smaller than 20");
+            }else if (number == 20)
+            {
+                Console.WriteLine("Random Number equals to 20");
+            }
+            else
+            {
+                Console.WriteLine("Random Number is bigger than 20");
+            }
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
